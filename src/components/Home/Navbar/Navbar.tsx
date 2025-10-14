f40fe5e96d491839fd81a97d039be4cba8791ab1
Md. Tahmid Chowhdhury <mdtahmidchowdhury12@gmail.com>
2025-10-15 01:19:21 +0600

    Navbar created

--- src/components/Home/Navbar/Navbar.tsx
+++ src/components/Home/Navbar/Navbar.tsx
@@ -1,21 +1,62 @@
-import React from 'react'
-import { MdDeliveryDining } from 'react-icons/md'
+"use client";
+import { useEffect, useState } from "react";
+import React from "react";
+import { MdDeliveryDining } from "react-icons/md";
+import { Navlinks } from "../../../../constants/constants";
+import Link from "next/link";
+import { HiBars3BottomRight } from "react-icons/hi2";
 
-const Navbar = () => {
-  return (
-    <div className="transition-all duration-200 h-[12vh] fixed w-full">
-      <div className="flex items-center h-full justify-between w-[90%] xl:w-[80%] mx-auto">
-        {/* Logo */}
-        <div className="flex items-center space-x-2">
-          <div className="w-10 h-10 bg-blue-950 dark:bg-white rounded-full flex items-center justify-center flex-col">
-            <MdDeliveryDining className="w-6 h-6 text-white dark:text-black"/>
-          </div>
-          <h1 className="text-xl hidden sm:block md:text-2xl text-black font-bold">Foodie</h1>
-        </div>
-        {/* 30:00 */}
-      </div>
-    </div>
-  )
+type Props = {
+  openNav: () => void;
 }
 
-export default Navbar
+const Navbar = ({ openNav }: Props) => {
+  const [navBg, setNavBg] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY >= 90) setNavBg(true);
+      if (window.scrollY < 90) setNavBg(false);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <div className={`transition-all ${navBg ? 'bg-white shadow-md' : 'fixed'} duration-200 h-[12vh] fixed w-full`}>
+      <div className="flex items-center h-full justify-between w-[90%] xl:w-[80%] mx-auto">
+        <div className="flex items-center space-x-2">
+          <div className="w-10 h-10 bg-blue-950 dark:bg-white rounded-full flex items-center justify-center flex-col">
+            <MdDeliveryDining className="w-6 h-6 text-white dark:text-black" />
+          </div>
+          <h1 className="text-xl hidden sm:block md:text-2xl text-black font-bold">
+            Foodie
+          </h1>
+        </div>
+        <div className="hidden lg:flex items-center space-x-10">
+          {Navlinks.map((link, index) => {
+            return (
+              <Link
+                key={index}
+                href={link.url}
+                className="text-black hover:text-green-700 font-bold transition-all duration-200"
+              >
+                <p>{link.label}</p>
+              </Link>
+            );
+          })}
+        </div>
+        <div className="flex items-center space-x-4">
+          <button className="bg-blue-950 px-8 py-2.5 text-white font-bold rounded-lg hover:bg-black transition-all duration-300 cursor-pointer">
+            Join Now
+          </button>
+          <HiBars3BottomRight onClick={openNav} className="w-8 h-8 cursor-pointer text-blue-950 lg:hidden" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
