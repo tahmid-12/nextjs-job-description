edb5978e17512c7e4017b7a5a94b14d2892e3824
Md. Tahmid Chowhdhury <mdtahmidchowdhury12@gmail.com>
2025-10-17 23:53:01 +0600

    Dark and light toggle added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,10 @@ import type { Metadata } from "next";
 import { Manrope } from "next/font/google";
 import "./globals.css";
 import ResponsiveNav from "@/components/Home/Navbar/ResponsiveNav";
+import Provider from "@/components/HOC/Provider";
 
 const font = Manrope({
-  weight: ["200","300", "400", "500", "600", "700", "800"],
+  weight: ["200", "300", "400", "500", "600", "700", "800"],
   subsets: ["latin"],
 });
 
@@ -19,12 +20,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${font.className} antialiased`}
-      >
-        <ResponsiveNav />
-        {children}
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${font.className} antialiased`}>
+        <Provider>
+          <ResponsiveNav />
+          {children}
+        </Provider>
       </body>
     </html>
   );
--- src/components/Home/Navbar/Navbar.tsx
+++ src/components/Home/Navbar/Navbar.tsx
@@ -5,10 +5,11 @@ import { MdDeliveryDining } from "react-icons/md";
 import { Navlinks } from "../../../../constants/constants";
 import Link from "next/link";
 import { HiBars3BottomRight } from "react-icons/hi2";
+import ThemeToggler from "@/components/Helper/ThemeToggler";
 
 type Props = {
   openNav: () => void;
-}
+};
 
 const Navbar = ({ openNav }: Props) => {
   const [navBg, setNavBg] = useState(false);
@@ -25,13 +26,17 @@ const Navbar = ({ openNav }: Props) => {
   }, []);
 
   return (
-    <div className={`transition-all ${navBg ? 'bg-white shadow-md' : 'fixed'} duration-200 h-[12vh] fixed w-full`}>
+    <div
+      className={`transition-all ${
+        navBg ? "bg-white dark:bg-gray-900 shadow-md" : "fixed"
+      } duration-200 h-[12vh] fixed w-full`}
+    >
       <div className="flex items-center h-full justify-between w-[90%] xl:w-[80%] mx-auto">
         <div className="flex items-center space-x-2">
           <div className="w-10 h-10 bg-blue-950 dark:bg-white rounded-full flex items-center justify-center flex-col">
             <MdDeliveryDining className="w-6 h-6 text-white dark:text-black" />
           </div>
-          <h1 className="text-xl hidden sm:block md:text-2xl text-black font-bold">
+          <h1 className="text-xl hidden sm:block md:text-2xl text-black dark:text-white font-bold">
             Foodie
           </h1>
         </div>
@@ -41,7 +46,7 @@ const Navbar = ({ openNav }: Props) => {
               <Link
                 key={index}
                 href={link.url}
-                className="text-black hover:text-green-700 font-bold transition-all duration-200"
+                className="text-black dark:text-white hover:text-green-700 dark:hover:text-green-400 font-bold transition-all duration-200"
               >
                 <p>{link.label}</p>
               </Link>
@@ -49,10 +54,14 @@ const Navbar = ({ openNav }: Props) => {
           })}
         </div>
         <div className="flex items-center space-x-4">
-          <button className="bg-blue-950 px-8 py-2.5 text-white font-bold rounded-lg hover:bg-black transition-all duration-300 cursor-pointer">
+          <button className="bg-blue-950 dark:bg-white dark:text-black dark:hover:bg-gray-200 px-8 py-2.5 text-white font-bold rounded-lg hover:bg-black transition-all duration-300 cursor-pointer">
             Join Now
           </button>
-          <HiBars3BottomRight onClick={openNav} className="w-8 h-8 cursor-pointer text-blue-950 lg:hidden" />
+          <ThemeToggler />
+          <HiBars3BottomRight
+            onClick={openNav}
+            className="w-8 h-8 cursor-pointer text-blue-950 lg:hidden dark:text-white"
+          />
         </div>
       </div>
     </div>
